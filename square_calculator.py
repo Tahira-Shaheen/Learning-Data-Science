0b04e7df930087aeb6f804c9fd33e0f26f83c6ae
Tahira-Shaheen <tahirafiaz11@gmail.com>
2024-11-26 11:53:08 +0500

    Added square calculator script
    Added square calculator script

---
+++ square_calculator.py
@@ -0,0 +1,6 @@
+# This script calculates the square of a number
+# Ask the user for input
+number = int(input("Enter a number"))
+square = number **2
+# Print the result
+print(f"The square of{number}is{square}")
